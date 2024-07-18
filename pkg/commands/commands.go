08cef6b03a4e999ea9454a303255fc8ad152211d
Aayush Shah <aayushshah1142@gmail.com>
2024-07-18 22:31:45 +0530

    implement ECHO

--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -9,7 +9,14 @@ func handlePing(args []resp.Value) resp.Value {
 
 	return resp.Value{DataType: resp.TypeString, Str: args[0].Bulk}
 }
+func handleEcho(args []resp.Value) resp.Value {
+	if len(args) != 1 {
+		return resp.Value{DataType: resp.TypeString, Str: "ERR: wrong number of arguments"}
+	}
+	return resp.Value{DataType: resp.TypeString, Str: args[0].Bulk}
+}
 
 var CommandHandler = map[string]func([]resp.Value) resp.Value{
 	"PING": handlePing,
+	"ECHO": handleEcho,
 }
